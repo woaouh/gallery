eb5cf7365b20ee18c703aa10c64483a0be0e8158
Vladimir <woaouh@outlook.com>
2020-12-25 11:17:28 +0200

    Moved Listener from Card to Gallery component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './Card.module.sass';
 
-import { fetchBigImage, toggleModal } from '../../redux/gallerySlice';
-
-export default function Card({ id, url }) {
-  const dispatch = useDispatch();
-  const [bigImage, setBigImage] = useState(null);
-
-  useEffect(() => {
-    if (bigImage) {
-      dispatch(fetchBigImage(id));
-    }
-  }, [bigImage, dispatch, id]);
-
-  function onCardClickHandler() {
-    setBigImage(id);
-    dispatch(toggleModal());
-  }
+export default function Card({ id, url, onClickHandler }) {
   return (
-    <div className={classes.Card} onClick={onCardClickHandler} onKeyUp={onCardClickHandler} role="button" tabIndex="0">
+    <div className={classes.Card} onClick={() => onClickHandler(id)} onKeyUp={() => onClickHandler(id)} role="button" tabIndex="0">
       <img src={url} alt="gallery content" />
     </div>
   );
@@ -29,4 +13,5 @@ export default function Card({ id, url }) {
 Card.propTypes = {
   id: PropTypes.number.isRequired,
   url: PropTypes.string.isRequired,
+  onClickHandler: PropTypes.func.isRequired,
 };
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Gallery.module.sass';
+
 import Card from '../Card/Card';
+
+import { fetchBigImage, toggleModal } from '../../redux/gallerySlice';
 
 export default function Gallery() {
   const images = useSelector(({ gallery }) => gallery.images);
+  const [currentId, setCurrentId] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (currentId) {
+      dispatch(fetchBigImage(currentId));
+    }
+  }, [dispatch, currentId]);
+
+  function onCardClickHandler(id) {
+    setCurrentId(id);
+    dispatch(toggleModal());
+  }
   return (
     <section className={classes.Gallery}>
       {images.map((image) => (
-        <Card key={image.id} url={image.url} id={image.id} />
+        <Card key={image.id} url={image.url} id={image.id} onClickHandler={onCardClickHandler} />
       ))}
     </section>
   );
